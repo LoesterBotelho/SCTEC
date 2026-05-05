c361788b5f3b4ae38daa7862acf111af29ffa359
Loester Botelho <156843598+LoesterBotelho@users.noreply.github.com>
2026-05-04 22:18:12 -0300

    criação de table

---
+++ ia-python/aula01.py
@@ -0,0 +1,15 @@
+# google collab
+
+
+def soma(a, b):
+    return a + b
+
+result = soma(2, 3)
+print("Resultado:", result)
+
+
+idade = 17
+nome = "Loester"
+
+print ("Olá, mundo!") # Texto Simples
+print(f"{nome} tem {idade} anos.")
